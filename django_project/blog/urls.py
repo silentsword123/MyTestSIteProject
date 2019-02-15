27c9e8bd21c74f3f9e21a9d5a14186cd8eee159b
zhihao <dengzhihao2018@gmail.com>
2019-02-14 23:44:32 -0500

    update profile function

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView
 app_name = 'blog'
 urlpatterns=[
-    path('',views.home,name='blog-home'),
-    path('about',views.about,name='blog-about'),
+    path('',PostListView.as_view(),name='blog-home'),
+    path('about/',views.about,name='blog-about'),
+    path('post/<int:pk>/',PostDetailView.as_view(),name='post-detail')
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView,DetailView,UpdateView,DeleteView
 # Create your views here.
 def home(request):
     context={
         'posts':Post.objects.all(),
     }
     return render(request,'blog/home.html',context)
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' #<app>/<model>_<viewtype>/html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+class PostDetailView(DetailView):
+    model = Post
+
 def about(request):
     context={
         'title':'About',
     }
-    return render(request,'blog/about.html',context )
+    return render(request,'blog/about.html',context)
+
+
